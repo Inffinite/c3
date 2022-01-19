086ffa3cf7d7c023b9c69835991a1b5e6020968a
Inffinite <louislaizer4@gmail.com>
2022-01-19 16:46:58 +0300

    :hammer: Favicon and fonts

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    weeeh
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+@import '../styles/Home.css';
+</style>
