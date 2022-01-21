caf85dcf84e6732ab71d0c47bfcb5d6bda83bf77
Inffinite <louislaizer4@gmail.com>
2022-01-21 15:36:59 +0300

    :fire: Mobile ui changes
    In hello and nav components

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="hello">
     <div class="h-details">
-      <div class="h-tag">Sustainability</div>
+      <div class="container">
+        <div class="h-tag">Sustainability</div>
+      </div>
       <div class="h-first">Act Responsibly</div>
       <div class="h-second">Think Sustainably</div>
 
       <div class="h-subtitle">
-        We identify, plan, manage and execute projects <br> that are
-        aligned with Sustainable Development Goals. 
+        We identify, plan, manage and execute projects 
+        that are aligned with <span class="green">Sustainable Development Goals.</span>
       </div>
 
       <div class="h-actions">
-        <div class="findout">Find out more</div>
-        <div class="h-pdf">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="pdf-icon"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
-              clip-rule="evenodd"
-            />
-          </svg>
+        <div class="container">
+          <div class="findout">Find out more</div>
+        </div>
+        <div class="container">
+          <div class="h-pdf">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="pdf-icon"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-          Download PDF
+            Download PDF
+          </div>
         </div>
       </div>
     </div>
     <div class="h-pic">
-      <img class="h-image" src="../assets/combo.png" alt="">
+      <img class="h-image" src="../assets/combo.png" alt="" />
     </div>
   </div>
 </template>
