9853404c240c8fae651f7a0547be860e4c5cb77e
Inffinite <louislaizer4@gmail.com>
2022-01-21 23:09:52 +0300

    :hammer: Focus component desktop ui

---
+++ src/components/Focus.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="focus">
+    <div class="focus-subtitle">Building the future</div>
+    <div class="focus-title">
+        <span class="thin">Our</span> Focus
+    </div>
+    <div class="f-list">
+        <div class="f-item">
+            <img class="f-item-img" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D" alt="">
+            <div class="f-item-title">
+                Education
+            </div>
+            <!-- <div class="f-item-subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div> -->
+        </div>
+        <div class="f-item">
+            <img class="f-item-img" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D" alt="">
+            <div class="f-item-title">
+                Education
+            </div>
+            <!-- <div class="f-item-subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div> -->
+        </div>
+        <div class="f-item">
+            <img class="f-item-img" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D" alt="">
+            <div class="f-item-title">
+                Education
+            </div>
+            <!-- <div class="f-item-subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div> -->
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "../styles/Focus.css";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <Navbar />
     <Hello />
+    <Focus/>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
 import Hello from "../components/Hello.vue";
+import Focus from '../components/Focus.vue'
 
 export default {
   components: {
     Navbar,
     Hello,
+    Focus
   },
 };
 </script>
