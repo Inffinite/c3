a0292227e2040d841b60984ee8b3e33ae7483fc5
Inffinite <louislaizer4@gmail.com>
2022-01-22 13:23:09 +0300

    :hammer: Focus component ui changes

--- src/components/Focus.vue
+++ src/components/Focus.vue
@@ -6,47 +6,47 @@
       <div class="f-item">
         <img
           class="f-item-img"
-          src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D"
-          alt=""
-        />
-        <div class="f-item-title">Education</div>
-        <!-- <div class="f-item-subtitle">
-                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
-            </div> -->
-      </div>
-      <div class="f-item">
-        <img
-          class="f-item-img"
-          src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          alt=""
-        />
-        <div class="f-item-title">Poverty Eradication</div>
-        <!-- <div class="f-item-subtitle">
-                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
-            </div> -->
-      </div>
-      <div class="f-item">
-        <img
-          class="f-item-img"
           src="https://images.unsplash.com/photo-1504598578017-40d9b776f1bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWZyaWNhbiUyMHBvdmVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
-        <div class="f-item-title">Climate Change</div>
-        <!-- <div class="f-item-subtitle">
-                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
-            </div> -->
+        <div class="f-item-title">No Poverty</div>
+        <div class="f-item-subtitle">
+          Access to basic human needs of health, education, sanitation.
+        </div>
+      </div>
+      <div class="f-item">
+        <img
+          class="f-item-img"
+          src="https://images.unsplash.com/photo-1609188076864-c35269136b09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YWZyaWNhbiUyMGRvY3RvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div class="f-item-title">Good Health</div>
+        <div class="f-item-subtitle">
+          Better, more accessible health systems to increase life-expectancy.
+        </div>
+      </div>
+      <div class="f-item">
+        <img
+          class="f-item-img"
+          src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D"
+          alt=""
+        />
+        <div class="f-item-title">Quality Education</div>
+        <div class="f-item-subtitle">
+          Inclusive education to enable upward social mobility and end poverty.
+        </div>
       </div>
 
       <div class="f-item">
         <img
           class="f-item-img"
-          src="https://images.unsplash.com/photo-1504598578017-40d9b776f1bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWZyaWNhbiUyMHBvdmVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          src="https://images.unsplash.com/photo-1556484687-30636164638b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGFmcmljYW4lMjBnZW5kZXIlMjBlcXVhbGl0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
         <div class="f-item-title">Gender Equality</div>
-        <!-- <div class="f-item-subtitle">
-                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
-            </div> -->
+        <div class="f-item-subtitle">
+          Education regardless of gender and advancement of equality laws.
+        </div>
       </div>
     </div>
     <div class="contain">
