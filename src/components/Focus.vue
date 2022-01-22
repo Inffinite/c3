a7e547f4397e3a5ec24c4cb223cc79c935b0c4cf
Inffinite <louislaizer4@gmail.com>
2022-01-22 09:58:43 +0300

    :fire: Focus component stable
    In mobile and desktop

--- src/components/Focus.vue
+++ src/components/Focus.vue
@@ -1,37 +1,56 @@
 <template>
   <div class="focus">
     <div class="focus-subtitle">Building the future</div>
-    <div class="focus-title">
-        <span class="thin">Our</span> Focus
-    </div>
+    <div class="focus-title"><span class="thin">Our</span> Focus</div>
     <div class="f-list">
-        <div class="f-item">
-            <img class="f-item-img" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D" alt="">
-            <div class="f-item-title">
-                Education
-            </div>
-            <!-- <div class="f-item-subtitle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      <div class="f-item">
+        <img
+          class="f-item-img"
+          src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D"
+          alt=""
+        />
+        <div class="f-item-title">Education</div>
+        <!-- <div class="f-item-subtitle">
+                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
             </div> -->
-        </div>
-        <div class="f-item">
-            <img class="f-item-img" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D" alt="">
-            <div class="f-item-title">
-                Education
-            </div>
-            <!-- <div class="f-item-subtitle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      </div>
+      <div class="f-item">
+        <img
+          class="f-item-img"
+          src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div class="f-item-title">Poverty Eradication</div>
+        <!-- <div class="f-item-subtitle">
+                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
             </div> -->
-        </div>
-        <div class="f-item">
-            <img class="f-item-img" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWZyaWNhbiUyMGVkdWNhdGlvbnxlbnwwfHwwfHw%3D" alt="">
-            <div class="f-item-title">
-                Education
-            </div>
-            <!-- <div class="f-item-subtitle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      </div>
+      <div class="f-item">
+        <img
+          class="f-item-img"
+          src="https://images.unsplash.com/photo-1504598578017-40d9b776f1bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWZyaWNhbiUyMHBvdmVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div class="f-item-title">Climate Change</div>
+        <!-- <div class="f-item-subtitle">
+                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
             </div> -->
-        </div>
+      </div>
+
+      <div class="f-item">
+        <img
+          class="f-item-img"
+          src="https://images.unsplash.com/photo-1504598578017-40d9b776f1bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWZyaWNhbiUyMHBvdmVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div class="f-item-title">Gender Equality</div>
+        <!-- <div class="f-item-subtitle">
+                Lorem ipsum sdfs sdfsdf sdfsdf sdfsdfhd
+            </div> -->
+      </div>
+    </div>
+    <div class="contain">
+      <button class="f-show-more">Show More</button>
     </div>
   </div>
 </template>
